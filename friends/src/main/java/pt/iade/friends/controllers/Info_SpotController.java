a8c86248833e6be6ec833217a06c903ffc8748f3
bolt40n <heliodivaldo2@hotmail.com>
2021-11-13 11:57:57 +0000

    Criei a funcao delete spot e users (usando um public boolean)

--- friends/src/main/java/pt/iade/friends/controllers/Info_SpotController.java
+++ friends/src/main/java/pt/iade/friends/controllers/Info_SpotController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pt.iade.friends.models.exceptions.NotFoundException;
 import pt.iade.friends.models.Info_Spot;
 import pt.iade.friends.models.repositories.Info_SpotRepository;
+import pt.iade.friends.models.responses.Response;
 
 @RestController
 @RequestMapping(path = "/api/info_spot")
@@ -52,13 +54,12 @@ public class Info_SpotController
 
     }
 
-    /*@DeleteMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response deleteUnit(@PathVariable int id) 
+    @DeleteMapping(path = "{internal_id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response deleteInfo_SpotById(@PathVariable int internal_id)
     {
-        logger.info("Deleting spot with id " + id);
-        // No verification to see if it exists
-        spotRepository.deleteById(id);
-        return new Response("Deleted spot with id " + id, null);
-        
-    }*/
+        logger.info("delete Info_Spot with internal_id: "+internal_id);
+        if(info_spotRepository.deleteInfo_SpotById(internal_id))
+        return new Response(internal_id+ "was deleted.", null);
+        else return new Response(internal_id+ "not found.", null);
+    }
 }
--- friends/src/main/java/pt/iade/friends/models/repositories/Info_SpotRepository.java
+++ friends/src/main/java/pt/iade/friends/models/repositories/Info_SpotRepository.java
@@ -4,5 +4,6 @@ import pt.iade.friends.models.Info_Spot;
 
 public interface Info_SpotRepository extends CrudRepository<Info_Spot,Integer> 
 {
-    
+    public Iterable<Info_Spot> getInfo_SpotById(int internal_id);
+    public boolean deleteInfo_SpotById(int internal_id);
 } 
