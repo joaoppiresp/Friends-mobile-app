0008c549252237ca0f8ead7aadb899a73c399fdd
bolt40n <heliodivaldo2@hotmail.com>
2021-11-17 18:26:34 +0000

    user.part 1

--- src/main/java/pt/iade/friends/controllers/UsersController.java
+++ src/main/java/pt/iade/friends/controllers/UsersController.java
@@ -1,22 +1,13 @@
 package pt.iade.friends.controllers;
-
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import pt.iade.friends.models.exceptions.NotFoundException;
 import pt.iade.friends.models.Users;
 import pt.iade.friends.models.repositories.UsersRepository;
-import pt.iade.friends.models.responses.Response;
 
 @RestController
 @RequestMapping(path = "/api/users")
@@ -25,38 +16,12 @@ public class UsersController
     private Logger logger = LoggerFactory.getLogger(UsersController.class);
     @Autowired
     private UsersRepository usersRepository;
+
     // get all users
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Users> getUsers() {
-        logger.info("Sending all Users");
+    public Iterable<Users> getUsers() 
+    {
+        logger.info("Sending all users");
         return usersRepository.findAll();
     }
-
-    // get user by id
-    @GetMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Users getUsers(@PathVariable int id) {
-        logger.info("Sending user");
-        Optional<Users> _users=usersRepository.findById(id);
-        if(_users.isEmpty()) throw 
-        new NotFoundException(id,id , "id");
-        else return _users.get() ;
-    }
-    /*
-    // save user
-    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Users saveUsers(@RequestBody Users user) {
-        Users saveUsers = usersRepository.save(user);
-    logger.info("Saving user with id"+id);
-    return saveUsers.getId();
-    }
-    */
-    // delete user
-    @DeleteMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response deleteUsers(@PathVariable int id) {
-        logger.info("Deleted user");
-        usersRepository.deleteById(id);
-        return new Response("Deleted user with id "+id, null);
-    }
-    
 }
-
