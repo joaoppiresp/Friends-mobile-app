0651b769d733932182599542690fd119ecd34198
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-11-17 15:11:02 +0000

    ..

--- src/main/java/pt/iade/friends/controllers/InfoSpotController.java
+++ src/main/java/pt/iade/friends/controllers/InfoSpotController.java
@@ -5,12 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.iade.friends.models.exceptions.NotFoundException;
 import pt.iade.friends.models.InfoSpot;
 import pt.iade.friends.models.repositories.InfoSpotRepository;
+import org.springframework.web.bind.annotation.RequestParam; 
 
 @RestController
 @RequestMapping(path = "/api/spots")
@@ -31,7 +32,7 @@ public class InfoSpotController
 
     // get spot by id
     @GetMapping(path= "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public InfoSpot getSpot(@PathVariable("id") Integer id) 
+    public InfoSpot getSpot(@RequestParam int id) 
     {
         logger.info("Sending spot");
         Optional<InfoSpot> _infospot = infoSpotRepository.findById(id);
