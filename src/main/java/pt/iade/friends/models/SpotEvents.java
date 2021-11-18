82aa6116fdb8e61893104607f5354c3ba35e0d7b
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-11-18 11:16:32 +0000

    ..

--- src/main/java/pt/iade/friends/models/SpotEvents.java
+++ src/main/java/pt/iade/friends/models/SpotEvents.java
@@ -1,6 +1,6 @@
 package pt.iade.friends.models;
 
-import java.sql.Date;
+import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,17 +15,17 @@ import javax.persistence.Table;
 public class SpotEvents {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "evnt_date") Date evntDate;
-    @Column(name = "event_type_id") private int evntTpId;
+    @Column(name = "evnt_date") Timestamp evntDate;
+    @Column(name = "event_type_id") private int evntTypeId;
     @Column(name = "spot_id") private int sptId;
-    @Column(name = "evnttype_id") private int evntTypeId;
+    @Column(name = "evnttype_id") private int evntTpId;
     
     public SpotEvents() {
         //empty constructor
     }
 
-    public Date getEvntDate(){return evntDate;}
-    public int getEvntTypeId(){return evntTpId;}
+    public Timestamp getEvntDate(){return evntDate;}
+    public int getEvntTypeId(){return evntTypeId;}
     public int getSpotId(){return sptId;}
-    public int getEvntTpId(){return evntTypeId;}
+    public int getEvntTpId(){return evntTpId;}
 }
