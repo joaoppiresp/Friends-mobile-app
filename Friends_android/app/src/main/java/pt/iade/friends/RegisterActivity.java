272d0607582289a15ec06fad10a8014908adb844
bolt40n <heliodivaldo2@hotmail.com>
2021-11-19 18:43:27 +0000

    fourth

--- Friends_android/app/src/main/java/pt/iade/friends/RegisterActivity.java
+++ Friends_android/app/src/main/java/pt/iade/friends/RegisterActivity.java
@@ -1,18 +1,23 @@
 package pt.iade.friends;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity
-{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secundary);
-        Intent intent = getIntent();
+//        Intent intent = getIntent();
+
+        Button travelButton = findViewById(R.id.travelButton);
+        travelButton.setOnClickListener(view -> {
+         Intent intent1 = new Intent(getApplicationContext(), MapFriend.class);
+         startActivity(intent1);
+        });
     }
 }
