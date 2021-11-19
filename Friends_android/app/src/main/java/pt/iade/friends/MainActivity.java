9ef1271dba2df37c5ffb72d6bc9fd21bf0e88854
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-11-19 21:44:39 +0000

    update main activity

--- Friends_android/app/src/main/java/pt/iade/friends/MainActivity.java
+++ Friends_android/app/src/main/java/pt/iade/friends/MainActivity.java
@@ -1,18 +1,33 @@
 package pt.iade.friends;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity{
 
     EditText username;
     EditText password;
     Button registerButton;
     Button dontRegisterButton1;
+    //locationservices
+    LocationManager locationManager;
+    LocationListener locationListener;
 
 
 
@@ -20,6 +35,58 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //location
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+
+
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+            }
+
+            @Override
+            public void onLocationChanged(@NonNull List<Location> locations) {
+
+            }
+
+            @Override
+            public void onFlushComplete(int requestCode) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+        else
+        {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0,
+                    0,
+                    locationListener
+            );
+        }
 
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
@@ -43,6 +110,24 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED)
+            {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        0,
+                        0,
+                        locationListener
+                );
+            }
+        }
     }
 }
 
