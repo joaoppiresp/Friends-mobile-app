628804e699035075e70f8b278cc6009e0dfe2019
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-11-23 20:52:45 +0000

    ..

--- src/main/java/pt/iade/friends/models/repositories/PricetypeRepository.java
+++ src/main/java/pt/iade/friends/models/repositories/PricetypeRepository.java
@@ -1,8 +1,9 @@
 package pt.iade.friends.models.repositories;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import pt.iade.friends.models.Pricetype;
 
 public interface PricetypeRepository extends CrudRepository<Pricetype, Integer> {
-        
+        Optional<Pricetype> getPrctpbyId(prctpId);
 }
