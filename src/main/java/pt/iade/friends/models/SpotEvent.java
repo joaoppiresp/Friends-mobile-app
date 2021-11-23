ffb01314c3110cd10cbf6d5d39e3e4b659662e35
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-11-23 17:54:53 +0000

    ..

--- src/main/java/pt/iade/friends/models/SpotEvent.java
+++ src/main/java/pt/iade/friends/models/SpotEvent.java
@@ -1,11 +1,17 @@
 package pt.iade.friends.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
+
 import java.sql.Timestamp;
 
 @Entity
@@ -17,8 +23,10 @@ public class SpotEvent
     @Column(name = "spot_evnt_id") public int evntId;
     @Column(name = "evnt_date") public Timestamp evntDate;
     @Column(name = "event_name") public String eventNm;
-    @Column(name = "spot_fk") public int spotFK;
-    @Column(name = "evnttype_fk ") public int evntTypeFK;
+    @ManyToOne( cascade = {CascadeType.PERSIST, CascadeType.MERGE}, targetEntity=InfoSpot.class )
+    @JoinColumn(name="spot_fk") public int spotFK;
+    @ManyToOne( cascade = {CascadeType.PERSIST, CascadeType.MERGE}, targetEntity=Eventtype.class )
+    @JoinColumn(name = "evnttype_fk ") public int evntTypeFK;
    
     public SpotEvent(){//empty const.
     };
