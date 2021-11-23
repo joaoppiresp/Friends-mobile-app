5d344d414d6d96ffde8dfca86ead5841ab0800a0
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-11-23 23:44:14 +0000

    ..

--- src/main/java/pt/iade/friends/models/repositories/PricingRepository.java
+++ src/main/java/pt/iade/friends/models/repositories/PricingRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PricingRepository extends CrudRepository<Pricing, Integer> {
- 
+    //TODO: REVER ESTA QUERY
     String getpricesQuery = "SELECT pricetype.prc_type AS type, "+
     "pricing.price AS amount, "+
     "infospot.spot_name AS spotName "+
     "From pricetype "+
-    "Inner Join pricing on picetype.prc_id=pricing.prctype_fk "+
+    "Inner Join pricing on pricetype.prc_id=pricing.prctype_fk "+
     "Inner Join infospot on pricing.spot_fk=infospot.spot_id "+
     "WHERE pricing.spot_fk = ?1";
 
