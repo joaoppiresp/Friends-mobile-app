f2043cb5e1e72840055dd72e12ff8970f28c9b9e
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-11-29 10:23:00 +0000

    ..

--- src/main/java/pt/iade/friends/controllers/SpotEventController.java
+++ src/main/java/pt/iade/friends/controllers/SpotEventController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.sql.Timestamp;
 import java.util.Optional;
-import pt.iade.friends.models.exceptions.NotFoundException;
+//import pt.iade.friends.models.exceptions.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
