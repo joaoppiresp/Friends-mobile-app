acd26cc795e7fde97e477afe8d0027ef9df49a0f
bolt40n <heliodivaldo2@hotmail.com>
2021-12-13 11:06:44 +0000

    weer

--- src/main/java/pt/iade/friends/models/repositories/UserInterestRepository.java
+++ src/main/java/pt/iade/friends/models/repositories/UserInterestRepository.java
@@ -8,17 +8,17 @@ import pt.iade.friends.models.Views.UserInterestView;
 public interface UserInterestRepository extends CrudRepository<UserInterest, Integer> 
 {
 
-    String getUserInterestQuery = "select user_nm AS NAME," +
-    "int_type AS INTEREST"+
-    "from users, userinterest, interests"+
-    "where users_fk = user_id and"+
-    "int_fk = interest_id";
+    String getUserInterestQuery = "select user_nm AS NAME, " +
+    "int_type AS INTEREST "+
+    "from users, userinterest, interests "+
+    "where users_fk = user_id and "+
+    "int_fk = interest_id ";
     
     //interest by user id
-    @Query(value = getUserInterestQuery + "WHERE userinterest.users_fk=usersFk", nativeQuery = true)
+    @Query(value = getUserInterestQuery + " WHERE userinterest.users_fk=usersFk ", nativeQuery = true)
     Iterable<UserInterestView> filteruserId(@Param("usersFk") int usersFk);
 
     //interest by interest id
-    @Query(value = getUserInterestQuery + "WHERE userinterest.int_fk=intFk", nativeQuery = true)
+    @Query(value = getUserInterestQuery + " WHERE userinterest.int_fk=intFk ", nativeQuery = true)
     Iterable<UserInterestView> filterinterestId(@Param("intFk") int intFk);
 }
