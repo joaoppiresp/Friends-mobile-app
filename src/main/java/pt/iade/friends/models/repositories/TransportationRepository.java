ad0dd30000f36edbe5efa82ff1d5f2986e3fde54
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-12-13 10:32:01 +0000

    delete_transportation

--- src/main/java/pt/iade/friends/models/repositories/TransportationRepository.java
+++
@@ -1,8 +0,0 @@
-package pt.iade.friends.models.repositories;
-import org.springframework.data.repository.CrudRepository;
-import pt.iade.friends.models.Transportation;
-
-public interface TransportationRepository extends CrudRepository<Transportation, Integer> 
-{
-
-}
--- src/main/java/pt/iade/friends/models/repositories/TransptypeRepository.java
+++
@@ -1,8 +0,0 @@
-package pt.iade.friends.models.repositories;
-import org.springframework.data.repository.CrudRepository;
-import pt.iade.friends.models.Transptype;
-
-public interface TransptypeRepository extends CrudRepository<Transptype, Integer> 
-{
-
-}
