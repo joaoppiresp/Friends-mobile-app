73277fe8e7f7de46868749be74d8950140fbda09
bolt40n <heliodivaldo2@hotmail.com>
2021-12-14 08:18:32 +0000

    Revert "mn"
    This reverts commit e884f495cbc074319871eec9a1f93d8dfb549be8.

--- src/main/java/pt/iade/friends/models/repositories/UserRepository.java
+++ src/main/java/pt/iade/friends/models/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package pt.iade.friends.models.repositories;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import pt.iade.friends.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> 
 {
+    Optional<User> findByNm(String nm);
+    Optional<User> findByEmail(String email);
 }
