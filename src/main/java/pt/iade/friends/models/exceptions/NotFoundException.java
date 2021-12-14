88ee8392f0890ac66a61be6d72047a526b987a2d
bolt40n <heliodivaldo2@hotmail.com>
2021-12-14 15:03:05 +0000

    qq

--- src/main/java/pt/iade/friends/models/exceptions/NotFoundException.java
+++ src/main/java/pt/iade/friends/models/exceptions/NotFoundException.java
@@ -8,8 +8,7 @@ public class NotFoundException extends RuntimeException
     private static final long serialVersionUID = 5405519104069955535L;
     public NotFoundException(String id, String elemType, String idName) 
     {
-        super(elemType+" with "+idName+" "+id+" not found."
-        +"\n"+"Research Differently.n");
+        super(elemType+" with "+idName+" "+id+" not found."+"\nResearch Differently");
     } 
 }
 
