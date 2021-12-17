4cdbbca8242447fe0b690a9a93696884564a7012
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-12-17 14:10:54 +0000

    ..

--- src/main/java/pt/iade/friends/models/repositories/InfoSpotRepository.java
+++ src/main/java/pt/iade/friends/models/repositories/InfoSpotRepository.java
@@ -10,14 +10,14 @@ import pt.iade.friends.models.InfoSpot;
 public interface InfoSpotRepository extends CrudRepository<InfoSpot, Integer> 
 {   
     String getSpotIdentifiersQuery = "SELECT spot_name AS sptName, spot_id AS spotId FROM infospot ";
-    String getSpotInfoQuery = "SELECT spot_name AS sptName, spot_address AS spotAddrs, contact_info AS spotContInfo ";
+    String getSpotInfoQuery = "SELECT spot_name AS sptName, spot_address AS sptAddress, contact_info AS sptContactInfo ";
     
     //getting Identifiers
     @Query(value=getSpotIdentifiersQuery + "WHERE infospot.spot_id >=:spotId", nativeQuery=true)
     Iterable<InfoSpotView> getAllIdentifiers(@Param("spotId") int spotId);
 
     //getting Info for searchView
-    @Query(value=getSpotInfoQuery + "WHERE infospot.spot_id >=:spotId", nativeQuery=true)
+    @Query(value=getSpotInfoQuery + "WHERE infospot.spot_id =:spotId", nativeQuery=true)
     Iterable<InfoSpotView2> getSpotInfo(@Param("spotId") int spotId);
 
 
