382f47a050c1faf4d2d990e0f0456c94f4a18867
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-12-17 12:36:57 +0000

    .

--- src/main/java/pt/iade/friends/models/repositories/InfoSpotRepository.java
+++ src/main/java/pt/iade/friends/models/repositories/InfoSpotRepository.java
@@ -8,11 +8,10 @@ import pt.iade.friends.models.Views.InfoSpotView;
 import pt.iade.friends.models.InfoSpot;
 public interface InfoSpotRepository extends CrudRepository<InfoSpot, Integer> 
 {   
-    String getSpotIdentifiersQuery = "SELECT spot_name, spot_id FROM infospot "+
-                                    "WHERE infospot.spot_id >=:spotId";
+    String getSpotIdentifiersQuery = "SELECT spot_name, spot_id FROM infospot ";
     
     //getting Identifiers
-    @Query(value=getSpotIdentifiersQuery + " ", nativeQuery=true)
+    @Query(value=getSpotIdentifiersQuery + "WHERE infospot.spot_id >=:spotId", nativeQuery=true)
     Iterable<InfoSpotView> getAllIdentifiers(@Param("spotId") int spotId);
 
 
