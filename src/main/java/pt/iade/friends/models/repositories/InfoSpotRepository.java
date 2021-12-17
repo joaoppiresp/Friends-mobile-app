733578965273a07f5f8562bc6b14dc5375584e30
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2021-12-17 12:30:46 +0000

    ..

--- src/main/java/pt/iade/friends/models/repositories/InfoSpotRepository.java
+++ src/main/java/pt/iade/friends/models/repositories/InfoSpotRepository.java
@@ -2,6 +2,8 @@ package pt.iade.friends.models.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import pt.iade.friends.models.Views.InfoSpotView;
 import pt.iade.friends.models.InfoSpot;
 public interface InfoSpotRepository extends CrudRepository<InfoSpot, Integer> 
@@ -11,7 +13,7 @@ public interface InfoSpotRepository extends CrudRepository<InfoSpot, Integer>
     
     //getting Identifiers
     @Query(value=getSpotIdentifiersQuery + " ", nativeQuery=true)
-    Iterable<InfoSpotView> getAllIdentifiers(int spotId);
+    Iterable<InfoSpotView> getAllIdentifiers(@Param("spotId") int spotId);
 
 
 
