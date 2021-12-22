04e27da27dc5ab3262421cb37bb968f9ee73a81d
bolt40n <heliodivaldo2@hotmail.com>
2021-12-22 17:53:20 +0000

    gh

--- src/main/java/pt/iade/friends/controllers/UserInterestController.java
+++ src/main/java/pt/iade/friends/controllers/UserInterestController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import pt.iade.friends.models.UserInterest;
 import pt.iade.friends.models.Views.UserInterestView;
+import pt.iade.friends.models.exceptions.NotFoundException;
 import pt.iade.friends.models.repositories.UserInterestRepository;
 
 @RestController
@@ -23,5 +29,16 @@ public class UserInterestController
     {
         logger.info(" Sending all user interest " +usersFk);
         return userInterestRepository.filterUserInterest(usersFk);
+    }
+
+    // get user by Id
+    @GetMapping(path ="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserInterest getUser(@PathVariable(value ="id") int id) 
+    {
+        logger.info("Sending User with id" +id);
+        Optional <UserInterest> _user=userInterestRepository.findById(id);
+        if(!_user.isPresent()) throw 
+        new NotFoundException("" +id,"UserInterest","id");
+        else return _user.get();
     }
 }
