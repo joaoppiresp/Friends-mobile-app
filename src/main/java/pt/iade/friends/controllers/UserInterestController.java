120d1faa76550e935e7c5fe016e66116086c9ca4
bolt40n <heliodivaldo2@hotmail.com>
2022-01-01 23:28:13 +0000

    1.7

--- src/main/java/pt/iade/friends/controllers/UserInterestController.java
+++ src/main/java/pt/iade/friends/controllers/UserInterestController.java
@@ -1,8 +1,11 @@
 package pt.iade.friends.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import pt.iade.friends.models.UserInterest;
 import pt.iade.friends.models.exceptions.NotFoundException;
 import pt.iade.friends.models.repositories.UserInterestRepository;
+import pt.iade.friends.models.responses.Response;
 
 @RestController
 @RequestMapping(path = "/api/userinterest")
@@ -41,4 +45,22 @@ public class UserInterestController
         new NotFoundException("" +id,"UserInterest","id");
         else return _userInterest.get();
     }
+
+     // save User Ineterest
+     @PostMapping(path ="/create", produces = MediaType.APPLICATION_JSON_VALUE)
+     public UserInterest saveUser(@RequestBody UserInterest userInterest) 
+     {
+        UserInterest savedUserInterest = userInterestRepository.save(userInterest);
+     logger.info("Saving User Interest with id");
+     return savedUserInterest;
+     }
+ 
+ // delete User
+     @DeleteMapping(path ="/delete/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
+     public Response deleteUser(@PathVariable(value ="id") int id) 
+     {
+         logger.info("Deleted User Interest with id"+id);
+         userInterestRepository.deleteById(id);
+         return new Response("Deleted User Interest with id" +id, null);
+     }
 }
