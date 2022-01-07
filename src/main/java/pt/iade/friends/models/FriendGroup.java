5d7f68e26fca2156e2e0d0c934e1ca92afb2bbb4
bolt40n <heliodivaldo2@hotmail.com>
2022-01-07 15:06:03 +0000

    23535

--- src/main/java/pt/iade/friends/models/FriendGroup.java
+++ src/main/java/pt/iade/friends/models/FriendGroup.java
@@ -12,15 +12,15 @@ public class FriendGroup
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "group_id") public int groupId;
-    @Column(name = "group_name") public String groupName;
+    @Column(name = "friendgroup_id") public int friendgroupId;
+    @Column(name = "group_name") public String name;
     @Column(name = "owner_id") public int ownerId;
     @Column(name = "friend_fk") public int friendFk;
 
     public FriendGroup(){}
     
-    public int getGroupId() {return groupId;}
-    public String getGroupName() {return groupName;}
+    public int getFriendGroupId() {return friendgroupId;}
+    public String getName() {return name;}
     public int getOwnerId() {return ownerId;}
     public int getFriendFk(){return friendFk;}
 }
