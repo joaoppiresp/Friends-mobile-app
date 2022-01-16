db9fb91cda86b7f0122f20c4b727b38d112e6f2b
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-01-16 22:54:26 +0000

    embeddable id class

--- src/main/java/pt/iade/friends/models/FriendGroupId.java
+++ src/main/java/pt/iade/friends/models/FriendGroupId.java
@@ -15,6 +15,37 @@ public class FriendGroupId implements Serializable {
     public FriendGroupId(){}
 
     public String getGpName() {return gpname;}
+    public void setGpName(String gpname){this.gpname=gpname;}
     public int getOwnerId() {return owner;}
+    public void setOwnerId(int owner){this.owner=owner;}
     public int getFriendFk(){return friendId;}
+    public void setFriendFk(int friendId){this.friendId=friendId;}
+
+    @Override
+    public int hashCode(){
+        final int prime = 31; //any number
+        int result = 1;
+        result = prime * result + ((gpname == null) ? 0 : gpname.hashCode());
+        result = prime * result + owner;
+        result = prime * result + friendId;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if (this == object)
+            return true;
+        if (object == null)
+            return false;
+        if (getClass() != object.getClass())
+            return false;
+        FriendGroupId other = (FriendGroupId) object;
+        if(owner != other.owner)
+            return false;
+        if(gpname == null){
+            if (other.gpname != null)
+                return false;
+        }else if(!this.gpname.equals(other.gpname)) return false;
+        return false;
+    }
 }
