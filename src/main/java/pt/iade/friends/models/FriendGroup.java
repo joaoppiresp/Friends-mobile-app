4b3871eb68ae2ea5a2506769a4e7c5cefb8db000
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-01-17 00:53:48 +0000

    ..

--- src/main/java/pt/iade/friends/models/FriendGroup.java
+++ src/main/java/pt/iade/friends/models/FriendGroup.java
@@ -1,7 +1,11 @@
 package pt.iade.friends.models;
 
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -11,6 +15,8 @@ public class FriendGroup
     @EmbeddedId
     private FriendGroupId id;
 
+
+    
     public FriendGroup(){}
     
 }
