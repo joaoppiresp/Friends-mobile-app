1e26d74bb27d3954e56010be16b9aa950cb3ac06
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-01-17 00:33:16 +0000

    ..

--- src/main/java/pt/iade/friends/controllers/FriendGroupController.java
+++ src/main/java/pt/iade/friends/controllers/FriendGroupController.java
@@ -33,10 +33,9 @@ public class FriendGroupController
 
     }
 
-    
     //save Group
     @PostMapping(path = "/{owner}/newgroups", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response saveFriendGroup(@RequestBody int owner, @RequestBody String gpname,@RequestBody int friends, @RequestBody String friendship) 
+    public Response saveFriendGroup(@PathVariable int owner, @RequestBody String gpname,@RequestBody int friends, @RequestBody String friendship) 
     {
         logger.info(" Saving friendgroup for user "+ owner);
         Integer inserted = friendgroupRepository.saveGroup(gpname,owner,friends,friendship);
