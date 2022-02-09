64d06725b9fa4cf188968b0e247d65005a0d1340
bolt40n <heliodivaldo2@hotmail.com>
2022-02-09 13:06:03 +0000

    asd

--- src/main/java/pt/iade/friends/controllers/FriendController.java
+++ src/main/java/pt/iade/friends/controllers/FriendController.java
@@ -1,21 +1,15 @@
 package pt.iade.friends.controllers;
 
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import pt.iade.friends.models.exceptions.NotFoundException;
-import pt.iade.friends.models.Friend;
 import pt.iade.friends.models.Views.FriendView;
 import pt.iade.friends.models.repositories.FriendRepository;
 import pt.iade.friends.models.responses.Response;
